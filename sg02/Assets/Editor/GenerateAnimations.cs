a28e61fc52256f8831acc84745058e8749c1d200
liangxc2014 <348096490@qq.com>
2014-12-22 23:47:31 +0800

    动画文件导出

---
+++ sg02/Assets/Editor/GenerateAnimations.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class GenerateAnimations
+{
+    //private static readonly string SourcePath = @"Assets\Resources\Shape\FORCE\";
+    //private static readonly string SourcePath = @"Assets\Resources\Shape\MAJOR\BODY\";
+    //private static readonly string SourcePath = @"Assets\Resources\Shape\MAJOR\HORSE\";
+    //private static readonly string SourcePath = @"Assets\Resources\Shape\MAJOR\WEAPON\";
+    private static readonly string SourcePath = @"Assets\Resources\Shape\TROOP\";
+    //private static readonly string SourcePath = @"Assets\Resources\Shape\FONTS\";
+
+	private static readonly string OutPath = @"Assets\Resources\Animations\";
+
+    //private static readonly int pattenLenght = 4;
+    private static readonly int pattenLenght = 2;
+
+    [MenuItem("Tools/生成动画文件")]
+    static void Execute()
+    {
+        DirectoryInfo di = new DirectoryInfo(SourcePath);
+        
+        if (di.Exists == false)
+            return;
+
+        FileInfo[] fileInfos = di.GetFiles();
+        bool flag = false;
+        string prefix = "-_-";
+        StreamWriter sw = null;
+
+        StreamWriter animSW = File.CreateText("Animations.txt");
+
+        for (int index = 0; index < fileInfos.Length; index++)
+        {
+            string path = fileInfos[index].FullName;
+            string fileName = Path.GetFileNameWithoutExtension(path);
+
+            if (!(path.EndsWith(".png")) || fileName.Length < pattenLenght + 1)
+                continue;
+
+            if (fileName.Length != 8)
+                continue;
+
+            flag = true;
+
+            if (fileName.StartsWith(prefix) == false)
+            {
+                if (sw != null)
+                {
+                    sw.Flush();
+                    sw.Close();
+
+                    animSW.WriteLine(OutPath + prefix);
+                }
+
+                prefix = fileName.Substring(0, fileName.Length - pattenLenght);    
+
+                sw = File.CreateText(OutPath + prefix + ".txt");
+            }
+
+            if (sw != null)
+            {
+                int idx = path.IndexOf("Resources");
+                string resPath = path.Substring(idx + 10, path.Length - idx - 14);
+                
+                sw.WriteLine(resPath);
+
+                EditorUtility.DisplayProgressBar("Processing", resPath, 1.0f * index / fileInfos.Length);
+            }
+        }
+
+        if (animSW != null)
+        {
+            animSW.Flush();
+            animSW.Close();
+        }
+
+        if (flag)
+            AssetDatabase.Refresh();
+
+        EditorUtility.ClearProgressBar();
+    }
+}
---
+++ sg02/Assets/Scripts/XML/Entity/XMLDataAnimations.cs
@@ -0,0 +1,18 @@
+
+
+public class XMLDataAnimations
+{
+	/// <summary>
+	/// 序号
+	/// </summary>
+	public int ID;
+	/// <summary>
+	/// 文件路径
+	/// </summary>
+	public string Path;
+	/// <summary>
+	/// 播放总时间
+	/// </summary>
+	public float AnimTime;
+}
+
